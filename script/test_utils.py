74cb90a64308362d9771317e114c7f78d06adc25
Michael Lamparski <diagonaldevice@gmail.com>
2021-03-18 18:30:39 -0400

    redundant utils
    I accidentally kept some unused utils that were
    written before SemigroupTree

--- script/test_utils.py
+++ script/test_utils.py
@@ -5,72 +5,6 @@ T = tp.TypeVar('T')
 G = tp.TypeVar('G')
 H = tp.TypeVar('H')
 
-def close_group(
-    generators: tp.Iterable[T],
-    func: tp.Callable[[T, T], T],
-    make_hashable: tp.Callable[[T], tp.Any] = lambda x: x,
-):
-    generators = list(generators)
-
-    yield generators[0]
-    all_seen = set(make_hashable(generators[0]))
-
-    for g in generators:
-        for old in all_seen:
-            new = func(g, old)
-            new_hashable = make_hashable(new)
-            while new_hashable not in all_seen:
-                yield new_hashable
-                all_seen.add(new_hashable)
-
-                new = func(g, new)
-                new_hashable = make_hashable(new)
-
-def set_update_new_items(
-        s: tp.MutableSet[T],
-        items: tp.Iterable[T],
-        make_hashable: tp.Callable[[T], tp.Any] = lambda x: x,
-) -> tp.Iterator[T]:
-    """ Adds items to a set (possibly mapping them through a function that makes them hashable)
-    and yields the items that are newly added. """
-    for item in items:
-        hashable = make_hashable(item)
-        if hashable not in s:
-            yield item
-            s.add(hashable)
-
-def func_bfs_groups(
-    roots: tp.Iterable[T],
-    edge_func: tp.Callable[[T], tp.Iterable[T]],
-    make_hashable: tp.Callable[[T], tp.Any] = lambda x: x,
-) -> tp.Iterator[tp.List[T]]:
-    """ In a directed, unweighted graph implicitly constructed by a given "outedges" function, perform a
-    breadth-first traversal beginning from ``roots`` and return groups of nodes by their proximity to any
-    of the nodes in ``roots``.
-    
-    E.g. first returns the roots themselves, then returns any new nodes that can be reached in one edge,
-    then any nodes that can be reached in 2 edges... """
-    all_seen = set()
-
-    new_items = list(set_update_new_items(all_seen, roots, make_hashable=make_hashable))
-    while new_items:
-        yield new_items
-        next_reachable_items = itertools.chain.from_iterable(edge_func(item) for item in new_items)
-        new_items = list(set_update_new_items(all_seen, next_reachable_items, make_hashable=make_hashable))
-
-def close_semigroup(
-    generators: tp.Iterable[T],
-    func: tp.Callable[[T], tp.Iterable[T]],
-    make_hashable: tp.Callable[[T], tp.Any] = lambda x: x,
-) -> tp.Iterator[T]:
-    """ Compute all items in the closure of a semigroup in a breadth-first manner from a set of generators. """
-    generators = list(generators)
-    yield from itertools.chain.from_iterable(func_bfs_groups(
-        roots=generators,
-        edge_func=(lambda node: func(g, node) for g in generators),
-        make_hashable=make_hashable,
-    ))
-
 GeneratorIndex = int
 NodeIndex = int
 
@@ -89,7 +23,6 @@ class SemigroupTree(tp.Generic[G]):
             func: tp.Callable[[G, G], G],
             make_hashable: tp.Callable[[G], tp.Any] = lambda x: x,
     ):
-        """ """
         self.generators = list(generators)
         self.generator_indices = []
         self.members = []
